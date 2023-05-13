4d3460b94d390feaac19ae2cbd148ddcc9897d77
Abu Suraj <abusurajbd@gmail.com>
2023-05-13 21:26:06 +0600

    add  click event

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heros',
@@ -8,9 +9,15 @@ import { Hero } from '../hero';
 })
 
 export class HerosComponent {
-  // hero = 'Windstorm';
+  heroes = HEROES;
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
+  }
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+    console.log(this.selectedHero);
+    
   }
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 12, name: 'Dr.Rahuk' },
+    { id: 13, name: 'Jabed' },
+    { id: 14, name: 'Suraj' },
+    { id: 15, name: 'Shamu' },
+    { id: 16, name: 'Rimu' },
+    { id: 17, name: 'Raju' },
+    { id: 18, name: 'Dr. IQ' },
+    { id: 19, name: 'Shumon' },
+    { id: 20, name: 'Tornado' }
+  ];
